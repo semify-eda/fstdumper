f1bd98f8b69392cb676c2837b890002779af746d
Leo Moser <leomoser99@gmail.com>
2021-08-12 09:53:31 +0200

    Adjust files to compile without errors

--- src/_pli_types.h
+++ src/_pli_types.h
@@ -19,10 +19,6 @@
  *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-# undef HAVE_INTTYPES_H
-
-#ifdef HAVE_INTTYPES_H
-
 /*
  * If the host environment has the stdint.h header file,
  * then use that to size our PLI types.
@@ -44,45 +40,5 @@ typedef unsigned char  PLI_UBYTE8;
 
 # define PLI_UINT64_FMT PRIu64
 
-#else
-
-/*
- * If we do not have the c99 stdint.h header file, then use
- * configure detection to guess the pli types ourselves.
- */
-
-# define SIZEOF_UNSIGNED_LONG_LONG 8
-# define SIZEOF_UNSIGNED_LONG 8
-# define SIZEOF_UNSIGNED 4
-
-#if SIZEOF_UNSIGNED >= 8
-typedef unsigned PLI_UINT64;
-typedef int      PLI_INT64;
-# define PLI_UINT64_FMT "u"
-#else
-# if SIZEOF_UNSIGNED_LONG >= 8
-typedef unsigned long PLI_UINT64;
-typedef          long PLI_INT64;
-#  define PLI_UINT64_FMT "lu"
-# else
-#  if SIZEOF_UNSIGNED_LONG_LONG > SIZEOF_UNSIGNED_LONG
-typedef unsigned long long PLI_UINT64;
-typedef          long long PLI_INT64;
-#   define PLI_UINT64_FMT "llu"
-#  else
-typedef unsigned long PLI_UINT64;
-typedef          long PLI_INT64;
-#   define PLI_UINT64_FMT "lu"
-#  endif
-# endif
 #endif
 
-typedef signed int     PLI_INT32;
-typedef unsigned int   PLI_UINT32;
-typedef signed short   PLI_INT16;
-typedef unsigned short PLI_UINT16;
-typedef char           PLI_BYTE8;
-typedef unsigned char  PLI_UBYTE8;
-#endif
-
-#endif /* PLI_TYPES_H */
